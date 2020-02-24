c896b574b04e35b699dfda1b31dd14a7aa9a6b8b
Nicolás Camacho Plazas <42525316+NicolasCamachoP@users.noreply.github.com>
2020-02-24 10:39:48 -0500

    Boilerplate Files

---
+++ example.cxx
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv )
+{
+  // Get command line arguments
+  if ( argc < 2 )
+  {
+  	std::cerr << "Usage: " << argv[ 0 ] << " image_file" << std::endl;
+    return( -1 );
+
+  } // fi
+
+  // Review given command line arguments
+  std::cout << "-------------------------" << std::endl;
+  for( int a = 0; a < argc; a++ )
+    std::cout << argv[ a ] << std::endl;
+  std::cout << "-------------------------" << std::endl;
+
+  // Read an image
+  Mat image;
+  image = imread( argv[1], 1 );
+
+  if ( !image.data )
+  {
+    std::cerr << "Error: No image data" << std::endl;
+    return( -1);
+  
+  } // fi
+  
+  //Write something
+  imwrite( basename + "_example.png", image);
+  return( 0 );
+}
+
+// eof - example.cxx
