009b5d9b7877df30afaed6b05294f5cf2cfcdcfb
Nicolás <nicolas-camacho@javeriana.edu.co>
2020-04-28 20:02:48 -0500

    4 Bonus points
    Variance, contrast, variable neighbors and segmented image implemented.

--- RegionGrowing/RegionGrow.cxx
+++ RegionGrowing/RegionGrow.cxx
@@ -4,118 +4,217 @@
 #include <vector>
 
 using namespace cv;
-const Point neighbors[8] = {Point(1,0),Point(1,-1),Point(-1,-1),Point(-1,0),Point(-1,1),Point(0,1),Point(0,-1),Point(1,1)};
+const Point neighbors[8] = {Point(1, 0), Point(1, -1), Point(-1, -1), Point(-1, 0), Point(-1, 1), Point(0, 1),
+                            Point(0, -1), Point(1, 1)};
 
 
-float average(Point aux, Mat &image_gray){
+float average(Point aux, Mat &image_gray, int numNeighbors) {
     float averVal = 0;
-    Scalar inten=image_gray.at<uchar>(aux.y, aux.x);
+    Scalar inten = image_gray.at<uchar>(aux.y, aux.x);
     int nNeigh = 0;
-    for(int i =0; i<8 ; i++){
+    for (int i = 0; i < 8; i++) {
+        if(numNeighbors == 4){
+            if(neighbors[i].x == -1 && neighbors[i].y == -1)
+                continue;
+            if(neighbors[i].x == 1 && neighbors[i].y == -1)
+                continue;
+            if(neighbors[i].x == -1 && neighbors[i].y == 1)
+                continue;
+            if(neighbors[i].x == 1 && neighbors[i].y == 1)
+                continue;
+        }
         Point actualPoint = aux + neighbors[i];
-        if(actualPoint.x < 0 || actualPoint.y <0 || actualPoint.x > image_gray.size().width-1 || actualPoint.y > image_gray.size().height-1)
+        if (actualPoint.x < 0 || actualPoint.y < 0 || actualPoint.x > image_gray.size().width - 1 ||
+            actualPoint.y > image_gray.size().height - 1)
             continue;
-        else{
+        else {
             nNeigh++;
-            inten=image_gray.at<uchar>(actualPoint.y, actualPoint.x);
-            averVal+=inten.val[0];
+            inten = image_gray.at<uchar>(actualPoint.y, actualPoint.x);
+            averVal += inten.val[0];
         }
     }
-    if(nNeigh!=0)
-        return averVal/nNeigh;
+    if (nNeigh != 0)
+        return averVal / nNeigh;
     return 0;
 }
 
 Point seedGenerator(Mat &image) {
-    Mat image_ycc, channels[3];
-    std::vector<float> varImage;
-    std::vector<Point> imagePoint;
-    float variance;
-    cvtColor(image, image_ycc, COLOR_RGB2YCrCb);
-    split(image_ycc, channels);
-
+    Mat histogram;
+    Point minPoint;
+    Point maxPoint;
+    double min, max;
+    int histSize = 256;
+    float range[] = {0, 256};
+    const float* histRange {range};
+    bool uniform = true;
+    bool accumulate = false;
+    calcHist(&image,1,0,Mat(), histogram,1,&histSize,&histRange,uniform,accumulate);
+    cv::minMaxLoc(histogram, &min, &max, &minPoint, &maxPoint);
+    int i,j;
+    uchar* p;
+    for( i = 0; i < image.rows; ++i)
+    {
+        p = image.ptr<uchar>(i);
+        for ( j = 0; j < image.cols; ++j)
+        {
+            if(p[j] == maxPoint.x)
+            {
+                return Point(i, j);
+            }
+        }
+    }
+    return maxPoint;
 }
-float variance(Point aux, Mat &channel){
-    return 0.1;
+
+float variance(Point aux, Mat &image_gray , int numNeighbors) {
+    float auxVal = 0;
+    float averVal = average(aux, image_gray, numNeighbors);
+    Scalar inten = image_gray.at<uchar>(aux.y, aux.x);
+    int nNeigh = 0;
+    for (int i = 0; i < 8; i++) {
+        if(numNeighbors == 4){
+            if(neighbors[i].x == -1 && neighbors[i].y == -1)
+                continue;
+            if(neighbors[i].x == 1 && neighbors[i].y == -1)
+                continue;
+            if(neighbors[i].x == -1 && neighbors[i].y == 1)
+                continue;
+            if(neighbors[i].x == 1 && neighbors[i].y == 1)
+                continue;
+        }
+        Point actualPoint = aux + neighbors[i];
+        if (actualPoint.x < 0 || actualPoint.y < 0 || actualPoint.x > image_gray.size().width - 1 ||
+            actualPoint.y > image_gray.size().height - 1)
+            continue;
+        else {
+            nNeigh++;
+            inten = image_gray.at<uchar>(actualPoint.y, actualPoint.x);
+            auxVal+= (inten.val[0]-averVal)*(inten.val[0]-averVal);
+        }
+    }
+    if (nNeigh != 0)
+        return auxVal/nNeigh;
+    return 0;
 }
 
-int contrast(std::vector<int> points){
-    /*int max=0;
-    int min=0;
-    max=*std::max_element(points.begin(),points.end());
-    min=*std::min_element(points.begin(),points.end());
-    return max-min;*/
-    return 1;
+int contrast(Point aux, Mat &image_gray, int numNeighbors) {
+    int max = -1;
+    int min = 256;
+    Scalar inten;
+    for (int i = 0; i < 8; i++) {
+        if(numNeighbors == 4){
+            if(neighbors[i].x == -1 && neighbors[i].y == -1)
+                continue;
+            if(neighbors[i].x == 1 && neighbors[i].y == -1)
+                continue;
+            if(neighbors[i].x == -1 && neighbors[i].y == 1)
+                continue;
+            if(neighbors[i].x == 1 && neighbors[i].y == 1)
+                continue;
+        }
+        Point actualPoint = aux + neighbors[i];
+        if (actualPoint.x < 0 || actualPoint.y < 0 || actualPoint.x > image_gray.size().width - 1 ||
+            actualPoint.y > image_gray.size().height - 1)
+            continue;
+        else {
+            inten = image_gray.at<uchar>(actualPoint.y, actualPoint.x);
+            if(inten.val[0]>max)
+                max = inten.val[0];
+            if(inten.val[0]<min)
+                min = inten.val[0];
+        }
+    }
+    if (max != -1 && min != 256)
+        return max-min;
+    return 0;
 }
 
-std::vector<Point> Grow(Mat &image_gray, Point &seed, float Thresh){
+std::vector<Point> Grow(Mat &image_gray, Point &seed, float Thresh, char criterion, int numNeighbors) {
     Vector<int> points;
     Point aux;
     vector<Point> region;
     vector<vector<bool>> verified;
-    for(int i =0; i<image_gray.size().height ; i++){
-        verified.push_back(std::vector<bool> (image_gray.size().width, false));
+    for (int i = 0; i < image_gray.size().height; i++) {
+        verified.push_back(std::vector<bool>(image_gray.size().width, false));
     }
-    std::queue <Point> active_front;
+    std::queue<Point> active_front;
     active_front.push(seed);
     image_gray.at<int>(seed.y, seed.x);
     //Growing algorithm
-    std::cout<<"Grow -> Antes del while"<< std::endl;
-    while(!active_front.empty()){
-        aux=active_front.front();
-        if(verified[aux.y][aux.x]){
+    std::cout << "Grow -> Antes del while" << std::endl;
+    while (!active_front.empty()) {
+        aux = active_front.front();
+        if (verified[aux.y][aux.x]) {
             active_front.pop();
             continue;
         }
         region.push_back(aux);
-        for(int i =0; i<8 ;i++){
+        for (int i = 0; i < 8; i++) {
+            if(numNeighbors == 4){
+                if(neighbors[i].x == -1 && neighbors[i].y == -1)
+                    continue;
+                if(neighbors[i].x == 1 && neighbors[i].y == -1)
+                    continue;
+                if(neighbors[i].x == -1 && neighbors[i].y == 1)
+                    continue;
+                if(neighbors[i].x == 1 && neighbors[i].y == 1)
+                    continue;
+            }
             Point actualPoint = aux + neighbors[i];
             //Check image limits
-            if(actualPoint.x < 0 || actualPoint.y <0 || actualPoint.x > image_gray.size().width-1 || actualPoint.y > image_gray.size().height-1){
+            if (actualPoint.x < 0 || actualPoint.y < 0 || actualPoint.x > image_gray.size().width - 1 ||
+                actualPoint.y > image_gray.size().height - 1) {
 
                 continue;
-            }
-
-            else{
-                if(std::abs((average(actualPoint, image_gray)-average(aux, image_gray)))<=Thresh){
-                    active_front.push(actualPoint);
+            } else {
+                if(criterion == 'a'){
+                    if (std::abs((average(actualPoint, image_gray, numNeighbors) - average(aux, image_gray, numNeighbors))) <= Thresh) {
+                        active_front.push(actualPoint);
+                    }
+                }else if(criterion == 'c'){
+                    if (std::abs((contrast(actualPoint, image_gray , numNeighbors) - contrast(aux, image_gray, numNeighbors))) <= Thresh) {
+                        active_front.push(actualPoint);
+                    }
+                }else if(criterion == 'v'){
+                    if (std::abs((variance(actualPoint, image_gray, numNeighbors) - variance(aux, image_gray, numNeighbors))) <= Thresh) {
+                        active_front.push(actualPoint);
+                    }
                 }
             }
         }
         verified[aux.y][aux.x] = true;
         active_front.pop();
     }
-    std::cout<<"Grow -> Después del while"<< std::endl;
+    std::cout << "Grow -> Después del while" << std::endl;
     return region;
 
 }
-int main(int argc, char** argv )
-{
+
+int main(int argc, char **argv) {
     // Get command line arguments
-    if ( argc < 5 )
-    {
-        std::cerr << "Usage: " << argv[ 0 ] << " image_file " <<  " x_seed_coordinates/-1 (-1 for automatic seed) " <<  " y_seed_coordinates/-1 (-1 for automatic seed) " << " umbral "<<std::endl;
-        return( -1 );
+    if (argc < 7) {
+        std::cerr << "Usage: " << argv[0] << " image_file " << " x_seed_coordinates/-1 (-1 for automatic seed) "
+                  << " y_seed_coordinates/-1 (-1 for automatic seed) " << " umbral "
+                  << "p/v/c (p for average, v for variance or c for contrast) " << " 4/8 (number of neighbors)" << std::endl;
+        return (-1);
     } // fi
 
     // Review given command line arguments
     std::cout << "-------------------------" << std::endl;
-    for( int a = 0; a < argc; a++ )
-        std::cout << argv[ a ] << std::endl;
+    for (int a = 0; a < argc; a++)
+        std::cout << argv[a] << std::endl;
     std::cout << "-------------------------" << std::endl;
 
     // Read an image
     Mat image;
-    image = imread( argv[1], 1 );
-    //PARA HACERLO AUTOMÁTICO SE PUEDEN ELEGIR LOS PUNTOS CON MAYOR INTENSIDAD DEL HISTOGRAMA
+    image = imread(argv[1], 1);
 
     //Saving Thresh (umbral)
     int umbral = std::atoi(argv[4]);
 
-    if ( !image.data )
-    {
+    if (!image.data) {
         std::cerr << "Error: No image data" << std::endl;
-        return( -1);
+        return (-1);
 
     } // fi
 
@@ -127,47 +226,58 @@ int main(int argc, char** argv )
     //Setting seed point
     Point seed;
     seed = Point(std::atoi(argv[2]), std::atoi(argv[3]));
-    if(seed.x == -1 || seed.y == -1){
-        if(seed.y >0 || seed.x > 0){
-            std::cerr<< "Invalid seed, for automatic seed both need to be -1 ..."<<std::endl;
+    if (seed.x == -1 || seed.y == -1) {
+        if (seed.y > 0 || seed.x > 0) {
+            std::cerr << "Invalid seed, for automatic seed both need to be -1 ..." << std::endl;
         }
-        std::cout<< "Automatic seed enabled..."<< std::endl;
+        std::cout << "Automatic seed enabled..." << std::endl;
         seed = seedGenerator(image_gray);
     } else {
-        if(seed.x > image.size().width-1 || seed.x > image.size().height-1){
-            std::cerr << "Seed point is outside image limits ..." <<std::endl;
+        if (seed.x > image.size().width - 1 || seed.x > image.size().height - 1) {
+            std::cerr << "Seed point is outside image limits ..." << std::endl;
         }
     }
 
+    //Select criterion to compare
+    char criterion = *argv[5];
+    if(criterion != 'a' && criterion != 'c' && criterion != 'v'){
+        std::cerr << "Sixth argument must be c for contrast, a for average or v for variance... "<< std::endl;
+    }
 
-   /* imshow("asdfs", image_gray);
-    waitKey();*/
+    //Saving number of neighbors
+    int nNeighbors = std::atoi(argv[6]);
+    if(nNeighbors != 4 && nNeighbors != 8){
+        std::cerr<< "Number of neighbors must be 4 or 8"<<std::endl;
+    }
 
     //Growing
-    std::cout<<"Va a entrar"<< std::endl;
-    vector<Point> region = Grow(image_gray, seed, umbral);
-    std::cout<<"Va a salir"<< std::endl;
+    std::cout << "Va a entrar" << std::endl;
+    vector<Point> region = Grow(image_gray, seed, umbral, criterion, nNeighbors);
+    std::cout << "Va a salir" << std::endl;
 
     //Fill object
-    Mat segmented = Mat::zeros(image.size(), CV_8UC3);
-    cvtColor(segmented, segmented, COLOR_RGB2GRAY);
+    Mat etiquetada = Mat::zeros(image.size(), CV_8UC3);
+    Mat segmentada = Mat::zeros(image.size(), CV_8UC3);
+    cvtColor(etiquetada, etiquetada, COLOR_RGB2GRAY);
+    cvtColor(segmentada, segmentada, COLOR_RGB2GRAY);
 
 
-    std::cout<<region.size()<<std::endl;
-    for(int i = 0; i<region.size();i++){
-        //std::cout<<"Punto a agregar x="<<region[i].x<< " y="<<region[i].x<<std::endl;
-        if(region[i].x==10 && region[i].y== 10){
-            std::cout<<"Si el resultado es extraño, se recomienda elegir un umbral menor..."<<std::endl;
+    std::cout << region.size() << std::endl;
+    for (int i = 0; i < region.size(); i++) {
+        if (region[i].x == 10 && region[i].y == 10) {
+            std::cout << "Si el resultado es extraño, se recomienda elegir un umbral menor..." << std::endl;
         }
-        segmented.at<uchar>(region[i].y,region[i].x ) = 255;
+        etiquetada.at<uchar>(region[i].y, region[i].x) = 255;
+        segmentada.at<uchar>(region[i].y, region[i].x) = image_gray.at<uchar>(region[i].y, region[i].x);
     }
 
     //Write something
-    std::stringstream ss ( argv[ 1 ] );
+    std::stringstream ss(argv[1]);
     std::string basename;
-    getline( ss, basename, '.' );
-    imwrite( basename + "_seg.png", segmented);
-    return( 0 );
+    getline(ss, basename, '.');
+    imwrite(basename + "_etiquetada.png", etiquetada);
+    imwrite(basename + "_segmentada.png", segmentada);
+    return (0);
 }
 
 // eof - RegionGrow.cxx
